d5e12492c458e5b5cffde9202b8a1c98874e91eb
Nando-Shen <sjm13128427700@gmail.com>
2023-08-06 00:15:11 +0800

    init

--- train.py
+++ train.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
     parser.add_argument('--world_size', default=4, type=int, help='world size')
     parser.add_argument('--batch_size', default=8, type=int, help='batch size')
     parser.add_argument('--data_path', type=str, help='data path of vimeo90k')
-    parser.add_argument('--config')
+    parser.add_argument('--config', type=str, help='config')
 
     args = parser.parse_args()
 
