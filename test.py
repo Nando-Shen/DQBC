1deb30cdcd73761e552253a7e4b3b7f32c62831f
Nando-Shen <sjm13128427700@gmail.com>
2023-08-18 16:51:38 +0800

    vis

--- test.py
+++ test.py
@@ -5,36 +5,79 @@ import argparse
 from datas.utils import imread_rgb
 import torchvision.transforms.functional as TF
 from PIL import Image
+from torchvision import transforms
+from torchvision.utils import save_image as imwrite
 import numpy as np
-import os 
+import os
 
-if __name__=='__main__':
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config',help='.yaml config file path')
-    parser.add_argument('--gpu_id',type=int,default=0)
-    parser.add_argument('--im0',type=str)
-    parser.add_argument('--im1',type=str)
-    parser.add_argument('--output_dir',type=str)
-    args = parser.parse_args()
-    cfg_file = args.config
-    dev_id = args.gpu_id
-    torch.cuda.set_device(dev_id)
+input_dir = '/home/kuhu6123/jshe2377/sim_keras_init'
+result_dir = '/home/kuhu6123/jshe2377/csi_vis_result'
+transforms = transforms.Compose([
+                transforms.ToTensor()
+            ])
+device = torch.device('cuda')
 
-    cfg = make_config(cfg_file, launch_experiment=False)
-    
-    print(model_profile(cfg.model))
-    
-    model = make_model(cfg.model)
-    model.cuda()
+
+def read(idx):
+    data_list = []
+    img0 = os.path.join(input_dir, '{}.jpg'.format(idx))
+    img1 = os.path.join(input_dir, '{}.jpg'.format(idx+2))
+
+    points14 = os.path.join(input_dir, '{}-{}points14.jpg'.format(idx,idx+2))
+    points12 = os.path.join(input_dir, '{}-{}points12.jpg'.format(idx,idx+2))
+    gt = os.path.join(input_dir, '{}.jpg'.format(idx+1))
+    data_list.extend([img0, img1, points14, points12, gt])
+    images = [Image.open(pth) for pth in data_list]
+    size = (384, 192)
+    images = [transforms(img_.resize(size)).unsqueeze(0) for img_ in images]
+
+    return images
+
+def test(images, idx):
+    print('Evaluating for {}'.format(idx))
     model.eval()
+    torch.cuda.empty_cache()
 
     with torch.no_grad():
-        im0 = TF.to_tensor(imread_rgb(args.im0))[None].cuda()
-        im1 = TF.to_tensor(imread_rgb(args.im1))[None].cuda()
-        pred = model(im0,im1)['final']
-        pred = pred[0].cpu().clamp(0.0, 1.0).numpy().transpose(1, 2, 0)*255
-        Image.fromarray(np.uint8(pred)).save(os.path.join(args.output_dir,'interp.png'))
+
+        images = [img_.to(device) for img_ in images]
+        points = torch.cat([images[3]], dim=1)
+        out = model(images[0], images[1], points, None)
+
+        imwrite(images[0], result_dir + '/{}csi.jpg'.format(idx))
+        imwrite(images[1], result_dir + '/{}csi.jpg'.format(idx+2))
+        imwrite(out[0], result_dir + '/{}csi.jpg'.format(idx+1))
+    return
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config',help='.yaml config file path')
+parser.add_argument('--gpu_id',type=int,default=0)
+parser.add_argument('--im0',type=str)
+parser.add_argument('--im1',type=str)
+parser.add_argument('--output_dir',type=str)
+args = parser.parse_args()
+cfg_file = args.config
+dev_id = args.gpu_id
+torch.cuda.set_device(dev_id)
+
+cfg = make_config(cfg_file, launch_experiment=False)
+
+print(model_profile(cfg.model))
+
+model = make_model(cfg.model)
+model.cuda()
+model.eval()
+
+for i in range(1, 199, 2):
+    images = read(i)
+    test(images,i)
+
+    # with torch.no_grad():
+    #     im0 = TF.to_tensor(imread_rgb(args.im0))[None].cuda()
+    #     im1 = TF.to_tensor(imread_rgb(args.im1))[None].cuda()
+    #     pred = model(im0,im1)['final']
+    #     pred = pred[0].cpu().clamp(0.0, 1.0).numpy().transpose(1, 2, 0)*255
+    #     Image.fromarray(np.uint8(pred)).save(os.path.join(args.output_dir,'interp.png'))
     
 
 
