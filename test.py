e9a7caa43cc95c21b1aaf6ccfd5982dad5a84907
Nando-Shen <sjm13128427700@gmail.com>
2023-08-18 17:03:49 +0800

    vis

--- test.py
+++ test.py
@@ -43,7 +43,7 @@ def test(images, idx):
         #     pred = pred[0].cpu().clamp(0.0, 1.0).numpy().transpose(1, 2, 0)*255
         #     Image.fromarray(np.uint8(pred)).save(os.path.join(args.output_dir,'interp.png'))
         out = out[0].cpu().clamp(0.0, 1.0).numpy().transpose(1, 2, 0) * 255
-        torch.tensor(out)
+        out = torch.tensor(out)
 
         imwrite(images[0], result_dir + '/{}csi.jpg'.format(idx))
         imwrite(images[1], result_dir + '/{}csi.jpg'.format(idx+2))
