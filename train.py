595b1ed63e29cecab4ecf23b7418123159be3bd9
shenjiamingnus <e1221616@u.nus.edu>
2024-07-30 21:00:24 +0800

    vis

--- train.py
+++ train.py
@@ -17,8 +17,6 @@ from dataset import VimeoDataset
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data.distributed import DistributedSampler
-from functools import partial
-
 import torch.optim as optim
 from test import evalvis
 
@@ -29,41 +27,13 @@ from models import make_model, model_profile
 device = torch.device("cuda")
 exp = os.path.abspath('.').split('/')[-1]
 
-def warm_cosine_scheduler(step,total_steps,peak=3e-4,bottom=3e-5,peak_step=2000):
-    # step ~ [0,total_steps)
-    step = step + 1
-    if step < peak_step:
-        mul = step / peak_step
-        return peak * mul
-    elif step >= total_steps:
-        return bottom
-    else:
-        mul = np.cos((step - peak_step) / (total_steps - peak_step) * math.pi) * 0.5 + 0.5
-        return (peak - bottom) * mul + bottom
-
-def multi_step_scheduler(step,initial,steps,decay):
-    lr = initial
-    for s in steps:
-        if step >= s:
-            lr *= decay
-        else:
-            break
-    return lr
 
 def make_optimizer(cfg, model):
     """ Create the optimizer and learning rate scheduler """
 
-    if cfg.lr.type == 'multi_step':
-        lr = cfg.lr.initial
-        scheduler = partial(multi_step_scheduler, initial=cfg.lr.initial, steps=cfg.lr.steps, decay=cfg.lr.decay)
-    elif cfg.lr.type == 'warm_cosine':
-        lr = 1e-6
-        scheduler = partial(warm_cosine_scheduler, total_steps=cfg.lr.total_steps, peak=cfg.lr.peak,
-                            bottom=cfg.lr.bottom, peak_step=cfg.lr.peak_step)
+    optimizer = optim.AdamW(model.parameters(), lr=2e-5, weight_decay=cfg.wdecay)
 
-    optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=cfg.wdecay)
-
-    return optimizer, scheduler
+    return optimizer
 
 
 def get_learning_rate(step):
